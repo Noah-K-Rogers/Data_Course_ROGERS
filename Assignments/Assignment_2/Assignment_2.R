4bd21714f3c030d413446945fd10b3146571013f
Noah-K-Rogers <ninjaguy77777@gmail.com>
2023-08-31 14:47:17 -0600

    assignment 2 and RProj

---
+++ Assignments/Assignment_2/Assignment_2.R
@@ -0,0 +1,18 @@
+# read csv files into variable
+csv_files <- c(list.files(path = "Data", pattern = ".csv",full.names = TRUE))
+#check the length of csv files
+length(csv_files)
+#save data
+df<-read.csv("Data/wingspan_vs_mass.csv")
+#check data
+head(df,n = 5)
+#find (and save) files that start with b
+b_list <- c(list.files(path = "Data", pattern = "^b", recursive = TRUE, full.names = TRUE))
+?list.files()
+#for loop to write first lines
+for (file_path in b_list){
+  print(readLines(file_path,n=1))
+}
+for (file_path in csv_files){
+  print(readLines(file_path,n=1))
+}
