f514a7e9867cc869b3594167b8a4d3a63f189938
Noah-K-Rogers <ninjaguy77777@gmail.com>
2023-09-16 13:56:44 -0600

    slightly better looking plots in exam 1

--- Exams/Exam_1/Exam_1.R
+++ Exams/Exam_1/Exam_1.R
@@ -25,13 +25,14 @@ levels(as.factor(A_states$Province_State))
 A_states$Last_Update <- as.Date(A_states$Last_Update)
 #III. Create a plot of that subset showing Deaths over time, with a separate facet for each state. (20 pts)####
 ggplot(A_states,aes(y=Deaths,x=Last_Update))+
+  labs(x="Date", y = "Total Deaths", title = "Cumulative Deaths over Time by State")+
 #Create a scatterplot
 geom_point()+
-
 #Add loess curves WITHOUT standard error shading
 geom_smooth(method = "loess", se = FALSE)+
 #Keep scales “free” in each facet
-facet_wrap(~ Province_State,scales = "free")
+facet_wrap(~ Province_State,scales = "free")+
+theme_minimal()
 
 
 #IV. (Back to the full dataset) Find the “peak” of Case_Fatality_Ratio for each state and save this as a new data frame object called state_max_fatality_rate. (20 pts)####
@@ -54,6 +55,9 @@ class(max_rates)
 #X-axis is Province_State
 #Y-axis is Maximum_Fatality_Ratio
 ggplot(max_rates, aes(x= reorder(Province_State, -Maximum_Fatality_Ratio), y=Maximum_Fatality_Ratio))+
+  labs(x="State", y="Maximum Fatality Ratio",title = "Death Ratios by State")+
+  theme_minimal()+
+  
 #bar plot
 geom_bar(stat = "identity")+
 #x-axis arranged in descending order, just like the data frame (make it a factor to accomplish this)
@@ -66,5 +70,8 @@ theme(axis.text.x =element_text(angle = 90) )
 death_over_time <- covid_data %>%
   group_by(Last_Update)%>%
   summarise(US_deaths = sum(Deaths))
+death_over_time$Last_Update <- as.Date(death_over_time$Last_Update)
 ggplot(death_over_time,aes(x=Last_Update,y = US_deaths))+
-  geom_point()
+  geom_line(linewidth = 1.5)+
+  theme_minimal()+
+  labs(x="Date", y="Deaths", title = "Cumulative Deaths in USA")
